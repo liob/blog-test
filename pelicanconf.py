9054440dc59faf2d9993293f8551fd538970fa28
liob <hbwinther@metalabs.de>
2014-04-22 09:29:26 +0200

    disable plugin better_figures_and_images

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,7 +52,8 @@ DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 
 PLUGIN_PATH = 'plugins'
-PLUGINS = ['pandoc_reader', 'better_figures_and_images']
+PLUGINS = ['pandoc_reader',]
+#PLUGINS = ['pandoc_reader', 'better_figures_and_images']
 RESPONSIVE_IMAGES = True
 PANDOC_ARGS = [ 
     '--bibliography=bibliography.bib',
